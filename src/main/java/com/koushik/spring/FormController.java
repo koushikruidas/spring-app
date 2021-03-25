5536281a4befdbca3bb9b9c9c8880b42710555cd
koushik-ruidas <koushikruidas@gmail.com>
2021-03-26 00:35:31 +0530

    added a form and showing the input message in display given by the user

---
+++ src/main/java/com/koushik/spring/FormController.java
@@ -0,0 +1,19 @@
+package com.koushik.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FormController {
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "form-page";
+	}
+	
+	@RequestMapping("/processForm")
+	public String processForm() {
+		return "form-view";
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/view/form-view.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>inside form</title>
+</head>
+<body>
+<h1>Hello Mr. ${param.studentName}</h1> 
+</body>
+</html>
